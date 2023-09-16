c04879f999cfc9ae47bf7e41b24f6f70dccf53ef
kdrbipul <kdrbipul@gmail.com>
2023-09-16 11:54:53 +0600

    bootstrap add and navbar included

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Footer from '../Components/Footer/Footer';
 import Header from '../Components/Header/Header';
 
 const Main = () => {
     return (
-        <div className='container'>
+        <div className=''>
             <Header />
+            <Outlet />
             <Footer />
         </div>
     );
