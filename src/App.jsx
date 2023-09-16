bee4816a3c5589d5d0f50c5bac78ff01fff96c85
kdrbipul <kdrbipul@gmail.com>
2023-09-16 11:47:06 +0600

    router install & setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Router/Router';
+
+
 
 function App() {
   
 
   return (
     <>
-      
+      <RouterProvider router={router}></RouterProvider>
     </>
   )
 }
---
+++ src/Layout/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../Components/Footer/Footer';
+import Header from '../Components/Header/Header';
+
+const Main = () => {
+    return (
+        <div className='container'>
+            <Header />
+            <Footer />
+        </div>
+    );
+};
+
+export default Main;
